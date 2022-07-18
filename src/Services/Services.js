2f250f09215b52eb427c426f2ed6c48115bc2c04
arshad657 <ahmedarshad8565@gmail.com>
2022-07-19 02:36:21 +0600

    third commit

--- src/Services/Services.js
+++ src/Services/Services.js
@@ -1,27 +1,49 @@
 import React from 'react'
 import { experimentalStyled as styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
+import Service from '../Home/Service/Service';
+import Fluride from '../images/fluoride.png'
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
 
+const services = [
+    {
+        name: 'florida',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ab expedita tempore eligendi sunt ratione mollitia dolore et deleniti. Blanditiis?',
+        img: Fluride, 
+},
+    {
+        name: 'florida',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ab expedita tempore eligendi sunt ratione mollitia dolore et deleniti. Blanditiis?',
+        img: Fluride, 
+},
+    {
+        name: 'florida',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ab expedita tempore eligendi sunt ratione mollitia dolore et deleniti. Blanditiis?',
+        img: Fluride, 
+},
+    {
+        name: 'florida',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ab expedita tempore eligendi sunt ratione mollitia dolore et deleniti. Blanditiis?',
+        img: Fluride, 
+}
+
+]
 function Services() {
   return (
     <Box sx={{ flexGrow: 1 }}>
         <Container>
       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-        {Array.from(Array(6)).map((_, index) => (
-          <Grid item xs={4} sm={4} md={4} key={index}>
-            <Item>xs=2</Item>
-          </Grid>
+
+
+        {services.map((service) => (
+            <Service 
+            key={service.name}
+            service={service}
+            
+            />
+          
         ))}
       </Grid>
       </Container>
